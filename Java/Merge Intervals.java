04510d00fb98cc69050b5a44845f24836a7ce81a
shawnfan <zhangfan.shawn@gmail.com>
2015-12-10 20:12:01 -0500

    Day4

--- Java/Merge Intervals.java
+++ Java/Merge Intervals.java
@@ -1,5 +1,24 @@
 扫描线+Count无敌手。注意start end把interval给合起来。
-count==0的时候，就是每次start end双数抵消的时候，就应该是一个interval的结尾。写个例子就知道了。
+count==0的时候，就是每次start end双数抵消的时候，就应该是一个interval的开头/结尾。写个例子就知道了。
+
+空间：O(2n) -> O(n)
+时间,priorityqueue: O(nlogn)
+
+记得怎么写comparator
+
+
+
+或者O(n)
+Collections.sort() on interval.start之后，试着跑一遍，按照merge的需求，把需要merge的地方续好，然后减掉多余的interval就好。
+Basic implementation
+/*
+    new Comparator<Object>(){
+        public int compare(obj1, obj2) {
+            return obj1.x - obj2.x;
+        }
+
+    }
+*/
 
 ```
 /*
@@ -15,12 +34,60 @@ Similar Problems: (H) Insert Interval, (E) Meeting Rooms (M) Meeting Rooms II
 */
 
 /*
+    Thoughts: 12.09.2015
+    Recap. Use O(1)
+
+    Sort by start time.
+    then it overlaps: check on pre.end and curr.start.
+    if overlaps: curr.start will be overlapped; also check on curr.end and pre.end, decide who ends this interval
+    
+    border case: null, return itself; or length==1, return.
+*/
+
+class Solution {
+    /**
+     * @param intervals: Sorted interval list.
+     * @return: A new sorted interval list.
+     */
+    public List<Interval> merge(List<Interval> intervals) {
+        if (intervals == null || intervals.size() <= 1) {
+            return intervals;
+        }
+
+        Collections.sort(intervals, new Comparator<Interval>(){
+            public int compare(Interval a, Interval b) {
+                return a.start - b.start;
+            }
+        });
+        Interval prev = intervals.get(0);
+        Interval curr;
+
+        for (int i = 1; i < intervals.size(); i++) {
+            curr  = intervals.get(i);
+            if (prev.end >= curr.start) {
+                if (prev.end <= curr.end) {
+                    prev.end = curr.end;
+                }
+                intervals.remove(i);
+                i--;
+            } else {
+                prev = curr;         
+            }
+        }
+
+        return intervals;
+    }
+
+}
+
+/*
 Thoughts:
 Again use scan line. Quite similar to Meeting Rooms, flight schedules... etc
 This one: when count ==0, make sure to keep track start and end, add into the rst
 When writing out example, whenever count==0, that indicates an end of a interval.
-*/
 
+HOWEVER, this uses O(n) space, while this problem requests O(1) space
+*/
 
 /**
  * Definition for an interval.
